2fc88fac9d5bcb7ae903f6945504719f8e1521c0
jlmoer <jlmoerdijk@gmail.com>
2022-02-21 17:46:56 +0100

    Suite Poke-context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+
 function Home() {
+    const [pokemon, setPokemon] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/1")
+            .then((res) => res.json())
+            .then((res) => {
+                setPokemon(res)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    if (isLoaded === false) {
+        console.log("load");
+        return <h3>Loading...</h3>
+    }
+
     return (
         <div>
             <h1>HOME</h1>
+            <p>Name: {pokemon.name}</p>
+            <p>Height: {pokemon.height}</p>
+            <p>Weight: {pokemon.weight}</p>
+            <p>Type:</p>
+            <ul>
+                {pokemon.types.map((type, index) => (
+                    <li key={index}>{type.type.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
